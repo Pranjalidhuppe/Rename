91f0c40cfbb8ae08a358caa3b324849b28de68dc
Pranjalidhuppe <pranjalidhuppe00@gmail.com>
2022-09-15 09:06:08 +0530

    Recursive function program

--- Demo1/src/com/practice/git/Example.java
+++ Demo1/src/com/practice/git/Example.java
@@ -1,10 +1,19 @@
 package com.practice.git;
 
 public class Example {
+//Print numbers from 1 to 100 without any loop
+	
+	public static void m1 (int num) {
+	
+		if(num <= 100) {
+			System.out.println(num);
+			m1 (num+1);
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello github");
+		m1(1);
 
 	}
 
